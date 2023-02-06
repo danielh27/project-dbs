9ce7b915e3614e33486a9e80fa1da09a03ab9b01
Daniel Herrera <daniel97hh@gmail.com>
2023-02-06 01:49:55 -0500

    create navbar

---
+++ app/components/nav_bar/base_component.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module NavBar
+  class BaseComponent < ApplicationComponent
+    private attr_reader :user
+
+    def initialize(user)
+      @user = user
+    end
+
+    def component
+      if user.present?
+        NavBar::UserComponent.new
+      else
+        NavBar::GuestComponent.new
+      end
+    end
+  end
+end
