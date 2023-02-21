c241f57775bee1267f9a34dabb0859307a063f21
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-02-21 05:14:42 -0500

    added limit when users update their username

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  before_save :update_attributes
-  before_update :update_nickname if :nickname_changed?
+  # before_save :update_attributes
+  # before_update :update_nickname, if: -> { nickname_changed? && nickname_was.present? }
+  before_update :update_nickname, if: :nickname_changed?
 
   def update_attributes
     update(nickname_last_updated: Date.current) if nickname_changed?
@@ -17,5 +18,7 @@ class User < ApplicationRecord
     return unless Date.current < nickname_last_updated.advance(days: 7)
 
     errors.add(:nickname, "Solo puede actualizar su nickname 1 vez por semana")
+
+    raise ActiveRecord::RecordInvalid.new(self)
   end
 end
