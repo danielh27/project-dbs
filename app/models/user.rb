8e3c9682c9d47b50da9651372471c8b40883589a
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-02-22 00:55:18 -0500

    added callbacks to put limit change nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,19 +6,18 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  # before_save :update_attributes
-  # before_update :update_nickname, if: -> { nickname_changed? && nickname_was.present? }
-  before_update :update_nickname, if: :nickname_changed?
+  before_create :set_nickname_last_updated
+  before_update :update_nickname_last_updated, if: :nickname_changed?
 
-  def update_attributes
-    update(nickname_last_updated: Date.current) if nickname_changed?
+  def set_nickname_last_updated
+    self.nickname_last_updated = Date.current if nickname.present?
   end
 
-  def update_nickname
-    return unless Date.current < nickname_last_updated.advance(days: 7)
+  def update_nickname_last_updated
+    return update(nickname_last_updated: Date.current) unless Date.current < nickname_last_updated.advance(days: 7)
 
     errors.add(:nickname, "Solo puede actualizar su nickname 1 vez por semana")
 
-    raise ActiveRecord::RecordInvalid.new(self)
+    raise ActiveRecord::RecordInvalid, self
   end
 end
