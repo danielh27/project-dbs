f71a7997bbea2247466362e94112964967d44387
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-02-22 21:39:02 -0500

    create address model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  has_many :addresses
 
   validates :nickname, presence: true, length: {minimum:10, message: "must be at least 10 characters long"}
 
---
+++ db/migrate/20230223023843_address.rb
@@ -0,0 +1,4 @@
+class Address < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
