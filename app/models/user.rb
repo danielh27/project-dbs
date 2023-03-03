e9f744dee0ef9986d6f44e84d9655e73d183b80c
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-03-02 19:34:17 -0500

    added login attrtibute

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  attr_accessor :login
 
   before_update :set_nickname_last_updated
   before_update :update_nickname_last_updated, if: :nickname_changed?
@@ -14,7 +15,7 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :address, allow_destroy: true, reject_if: :all_blank
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\S+@.+\.\S+/, message: "incorrect format" }
+  validates :email, presence: true, uniqueness: true, format: { with: /\S+@.+\.\S+/, message: "Formato incorrecto" }
 
 
   def set_nickname_last_updated
@@ -33,5 +34,13 @@ class User < ApplicationRecord
     return build_address if address.nil?
 
     address
+  end
+
+  def self.find_for_database_authentication(warden_condition)
+    conditions = warden_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).where(
+      ["lower(username) = :value", { value: login.strip.downcase }]
+    ).first
   end
 end
