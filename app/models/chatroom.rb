3e5d5091375ccbe412e2a0cb1be328ecb2690d3e
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-03-11 22:49:27 -0500

    added relations in models

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,4 @@
 class Chatroom < ApplicationRecord
   belongs_to :service
+  has_many :messages, dependent: :destroy
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :chatroom
-  belongs_to :sender
-  belongs_to :receiver
+  belongs_to :sender, class_name: "User"
+  belongs_to :receiver, class_name: "User"
 end
