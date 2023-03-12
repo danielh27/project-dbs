53a15d9a4a6c3114f1330b2d5e4dd8ac5a3642c0
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-03-11 22:51:31 -0500

    chatroom controller createsd

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,4 @@
+class ChatroomsController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'chatrooms/show'
   devise_for :users, path: '', path_names: { sign_in: 'login'}, controllers: { confirmations: 'session/confirmations', registrations: 'session/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
