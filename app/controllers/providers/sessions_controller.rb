c09cea6bd54201388bb1efd990c79fcfb494b684
Daniel Herrera <daniel97hh@gmail.com>
2023-03-25 16:56:06 -0500

    create provider controllers

---
+++ app/controllers/providers/sessions_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Providers::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
