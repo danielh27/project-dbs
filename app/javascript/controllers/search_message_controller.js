1b0a42dcd9e3337368f3e8cc535996df0eaeb2f2
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-03-27 01:13:55 -0500

    complete stimulus controller

--- app/javascript/controllers/search_message_controller.js
+++ app/javascript/controllers/search_message_controller.js
@@ -2,6 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search-message"
 export default class extends Controller {
+  static targets = ['form', 'messages', 'input']
+
   connect() {
+    console.log(this.formTarget)
+    console.log(this.inputTarget)
+    console.log(this.messagesTarget)
+  }
+
+  filter() {
+    const url = `${this.formTarget.action}?query=${this.inputTarget}`
+    fetch(url, { headers: { Accept: 'text/plain' }})
+      .then(response => response.text())
+      .then(data => {
+        console.log(data)
+      })
   }
 }
