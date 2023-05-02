a14a0068c7860d373a6de8c867f4a3487357a0c4
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-05-01 22:53:02 -0500

    messages have styles when insert them

--- app/javascript/controllers/chat_subscription_controller.js
+++ app/javascript/controllers/chat_subscription_controller.js
@@ -22,9 +22,14 @@ export default class extends Controller {
   }
 
   #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+    // Logic to know if the sender is the current_user
     const currentUserIsSender = this.currentUserIdValue === data.sender_id
+
+    // Creating the whole message from the `data.message` String
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
 
   #buildMessageElement(currentUserIsSender, message) {
