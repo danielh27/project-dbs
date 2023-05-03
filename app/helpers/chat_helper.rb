199e53125d562f552f31199d7626dbcc5a4e6a4a
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-05-02 23:52:56 -0500

    create methods in chat helper to modify alignment and color of message

--- app/helpers/chat_helper.rb
+++ app/helpers/chat_helper.rb
@@ -6,4 +6,12 @@ module ChatHelper
   def background_chat_list_item(chat_id, chat_list_item_id)
     chat_id == chat_list_item_id ? "bg-theme" : nil
   end
+
+  def sender?(sender, current_user)
+    sender.id == current_user.id
+  end
+
+  def set_class_name_by_message_sender(sender, current_user, first_class, second_class)
+    sender?(sender, current_user) ? first_class : second_class
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,8 +3,4 @@ class Message < ApplicationRecord
   belongs_to :sender, class_name: "User"
 
   validates :content, presence: true
-
-  def sender?(a_user)
-    sender.id == a_user.id
-  end
 end
