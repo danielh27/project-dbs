aae4b806b8be33837480b8c1df46fd71ec1a23a0
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-05-19 21:18:47 -0500

    set true or false if user is connected

--- app/channels/appearance_channel.rb
+++ app/channels/appearance_channel.rb
@@ -1,9 +1,23 @@
 class AppearanceChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    stream_from "appearances_channel"
+    update_user_status(true)
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+    update_user_status(false)
+  end
+
+  private
+
+  def update_user_status(online)
+    current_user.update(online:)
+    ActionCable.server.broadcast(
+      "appearances_channel",
+      user_id: current_user.id,
+      online:,
+    )
   end
 end
