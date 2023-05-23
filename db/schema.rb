ec741495f0f57b17d87a5cfb58b84507d7ae88d1
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-05-22 23:57:32 -0500

    add active column to users

--- db/schema.rb
+++ db/schema.rb
@@ -164,6 +164,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_221906) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.boolean "active", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
