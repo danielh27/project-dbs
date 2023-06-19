10c2b9d554b636e4e667123e1577db196f4663ec
Josedavid Ruiz Herrera <109052525+SekiroXtreme@users.noreply.github.com>
2023-06-19 18:36:06 -0500

    Homepage feature(Front) (#10)
    * initial homepage commit
    
    * add banner styles
    
    * add categories styles
    
    * minor fixes css rules
    
    * Minor optimizations on styles
    
    * add changes in application.scss
    
    * Minor optimizations
    
    * Minor fixes
    
    ---------
    
    Co-authored-by: Daniel Herrera <daniel97hh@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -149,7 +149,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_10_035643) do
     t.datetime "confirmation_sent_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
