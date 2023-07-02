4f13a209bb1204ad43483269ecf0409836e6386c
Deivid <JosedavidRH30@gmail.com>
2023-07-01 20:08:23 -0500

    devise-minor-bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :store_user_location!, if: :storable_location?
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   # private
@@ -18,19 +18,19 @@ class ApplicationController < ActionController::Base
     # store_location_for(:user, request.fullpath)
   # end
 
-  # protected
+  protected
 
-  # def configure_permitted_parameters
+  def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :nickname, :first_name, :last_name, :dni, :cellphone])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :nickname, :first_name, :last_name, :dni, :cellphone])
 
     # app/views/devise/sessions/new.html.erb
-    # devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    # devise_parameter_sanitizer.permit(:account_update, keys: [:email, :nickname, :cellphone, :avatar,
-                                                              # address_attributes: [:id, :main_address]])
-  # end
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :nickname, :cellphone, :avatar,
+                                                              address_attributes: [:id, :main_address]])
+  end
 
   # def after_sign_in_path_for(resource_or_scope)
   #   stored_location_for(resource_or_scope)
