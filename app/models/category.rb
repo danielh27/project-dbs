a0aede3741ed016fb09862d31c890f496570b752
Deivid <JosedavidRH30@gmail.com>
2023-07-27 17:19:52 -0500

    fix service_categories model error

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  has_many :service_categories
+  has_many :service_categories, dependent: :delete_all
   has_many :services, through: :service_categories
   has_one_attached :images
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,7 @@
 class Service < ApplicationRecord
   belongs_to :provider
   has_many_attached :images
-  has_many :service_categories
+  has_many :service_categories, dependent: :delete_all
   has_many :categories, through: :service_categories
   validates :name, :description, :slug, :categories, presence: true
   accepts_nested_attributes_for :categories, reject_if: :all_blank
