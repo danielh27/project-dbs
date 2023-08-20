61b31e91da9c118161cbf98e8d2d62837b05c53b
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-08-20 17:49:48 -0500

    create partial to message houre

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,6 +12,9 @@ class MessagesController < ApplicationController
         @chat,
         message: render_to_string(partial: "message", locals: { message: @message }),
         sender_id: @message.sender.id,
+        message_hour: render_to_string(partial: "message_hour", locals: { message: @message, chat: @chat }),
+        # me imagino pensar en hacer otro each with index para pasarle el index, tmb en la lista el chat de mas arriba
+        # debve ser el mas actuakl
         avatar: render_to_string(User::AvatarComponent.new(current_user), locals: { user: current_user }),
       )
       head :ok
