7cf30b71c32500dc8814f8415d159c76b3aa920e
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-08-25 21:46:40 -0500

    add new keys to broadcast, hour messages

--- app/helpers/chat_helper.rb
+++ app/helpers/chat_helper.rb
@@ -4,11 +4,11 @@ module ChatHelper
   end
 
   def background_chat_list_item(chat_id, chat_list_item_id)
-    chat_id == chat_list_item_id ? "bg-theme" : nil
+    chat_id.eql? chat_list_item_id ? "bg-theme" : nil
   end
 
   def sender?(sender, current_user)
-    sender.id == current_user.id
+    sender.id.eql? current_user.id
   end
 
   def set_class_name_by_message_sender(sender, current_user, first_class, second_class)
@@ -25,6 +25,12 @@ module ChatHelper
       (message_date_equal_to_previous_message?(message, messages, index) &&
       message_date_different_to_next_message?(message, messages, index) && sender?(message.sender, current_user)) ||
       !sender?(message.sender, current_user)
+      # esto ultimo linea 27 deberia esyar metodo de abajo
+  end
+
+  def show_hour_other_user?
+    # aqui es lo mismo de arriba menos la 27, hay que hacer las mismas comparaciones perocon el !sender?
+    # pensar si quiza puedo ponerlo arriba nomas donde esta el sender quiza hacer un (sender? || !sender?)
   end
 
   def message_date_different_to_previous_message?(message, messages, index)
@@ -36,6 +42,6 @@ module ChatHelper
   end
 
   def message_date_equal_to_previous_message?(message, messages, index)
-    message.created_at.strftime("%-d/%m/%Y %H:%M") == messages[index - 1].created_at.strftime("%-d/%m/%Y %H:%M")
+    message.created_at.strftime("%-d/%m/%Y %H:%M").eql? messages[index - 1].created_at.strftime("%-d/%m/%Y %H:%M")
   end
 end
