f012a4fe9ab78efd2347a430206a20b17e5498d1
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-09-12 20:41:49 -0500

    feat: :sparkles: add relation service and chat

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,6 +3,9 @@ class Service < ApplicationRecord
   has_many_attached :images
   has_many :service_categories, dependent: :delete_all
   has_many :categories, through: :service_categories
+  has_many :chats, dependent: :destroy
+
   validates :name, :description, :slug, :categories, presence: true
+
   accepts_nested_attributes_for :categories, reject_if: :all_blank
 end
