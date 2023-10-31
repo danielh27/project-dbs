17e3f43d5dc880cd038450865fb398847ef12761
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-10-31 01:06:49 -0500

    refactor: :art: refactor chats controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,21 +1,27 @@
 class ChatsController < ApplicationController
-  before_action :set_service, only: %i[show create]
+  before_action :set_service, only: %i[create]
   before_action :set_chat, only: %i[show]
 
   def show
     @message = Message.new
-    @chats = current_user.client_chats.left_joins(:messages).order("messages.created_at DESC NULLS LAST").uniq
-
-    if params[:query].present?
-      sql_query = " \
-        business_name iLIKE :query"
-
-      @chats = @chats.joins(:provider).where(sql_query, query: "%#{params[:query]}%")
-    end
+    @order_chats = current_user.client_chats.left_joins(:messages).order("messages.created_at DESC NULLS LAST")
+    @chats = @order_chats.uniq
+    query_filter
 
     respond_to do |format|
       format.html
       format.text { render partial: "chats/chat_list_item", locals: { chats: @chats }, formats: [:html] }
+    end
+  end
+
+  def my_chats
+    @message = Message.new
+    chat = Chat.find(params[:chat])
+
+    respond_to do |format|
+      format.text do
+        render partial: "chats/chat", locals: { chat:, message: @message }, formats: [:html]
+      end
     end
   end
 
@@ -26,7 +32,7 @@ class ChatsController < ApplicationController
     @chat.provider = @service.provider
     @chat.client = current_user
     if @chat.save
-      redirect_to service_chat_path(@service, @chat), notice: "Chat iniciado"
+      redirect_to chat_path(@chat), notice: "Chat iniciado"
     else
       render "services/show", status: :unprocessable_entity, alert: flash.now[:alert] = "No se pudo crear el chat"
     end
@@ -44,5 +50,14 @@ class ChatsController < ApplicationController
 
   def set_service
     @service = Service.find(params[:service_id].to_i)
+  end
+
+  def query_filter
+    return if params[:query].blank?
+
+    sql_query = " \
+      business_name iLIKE :query"
+
+    @chats = @order_chats.joins(:provider).where(sql_query, query: "%#{params[:query]}%").uniq
   end
 end
