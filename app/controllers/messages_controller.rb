dfa8f2e174b5b7ee30ad7b8bb116c638e4512e77
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-11-21 00:47:51 -0500

    chore: :wrench: esbuild configuration

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -28,17 +28,17 @@ class MessagesController < ApplicationController
 
   def broadcast_to_chat_channel(messages, index)
     ChatChannel.broadcast_to(@chat,
-        message: render_to_string(partial: "message", locals: { message: @message }),
-        sender_id: @message.sender.id,
-        is_same_sender: messages.last.sender == messages.last(2).first.sender,
-        actual_message_date: @message.created_at.strftime('%A %e %B %Y'),
-        actual_message_hour: @message.created_at.strftime("%-d/%m/%Y %H:%M"),
-        next_message_hour: messages[index + 1]&.created_at&.strftime("%-d/%m/%Y %H:%M"),
-        previous_message_hour: messages.one? ? "No exist" : messages[index - 1].created_at.strftime("%-d/%m/%Y %H:%M"),
-        message_hour_partial: render_to_string(partial: "message_hour", locals: { message: @message, chat: @chat,
-                                                                                  index: }),
-        can_show_hour: helpers.show_hour?(@message, messages, index),
-        can_show_date: helpers.show_date?(@message, messages, index),
-        avatar: render_to_string(User::AvatarComponent.new(current_user), locals: { user: current_user }))
+      message: render_to_string(partial: "message", locals: { message: @message }),
+      sender_id: @message.sender.id,
+      is_same_sender: messages.last.sender == messages.last(2).first.sender,
+      actual_message_date: @message.created_at.strftime('%A %e %B %Y'),
+      actual_message_hour: @message.created_at.strftime("%-d/%m/%Y %H:%M"),
+      next_message_hour: messages[index + 1]&.created_at&.strftime("%-d/%m/%Y %H:%M"),
+      previous_message_hour: messages.one? ? "No exist" : messages[index - 1].created_at.strftime("%-d/%m/%Y %H:%M"),
+      message_hour_partial: render_to_string(partial: "message_hour", locals: { message: @message, chat: @chat,
+                                                                                index: }),
+      can_show_hour: helpers.show_hour?(@message, messages, index),
+      can_show_date: helpers.show_date?(@message, messages, index),
+      avatar: render_to_string(User::AvatarComponent.new(current_user), locals: { user: current_user }))
   end
 end
