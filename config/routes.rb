c23c3250e085824f37c28e3159c86a44ea39a1c7
Segundo Rebaza <alvaro.rp1593@gmail.com>
2023-12-17 14:36:34 -0500

    feat: :recycle: modify styles and logic in views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'stimulus/hhh'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   devise_for :users, path: '', path_names: { sign_in: 'login' }, controllers: {
