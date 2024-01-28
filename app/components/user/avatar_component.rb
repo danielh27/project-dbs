b26cfae4e3af4a7ce4147cebc79e82389fa556ec
Daniel Herrera <76617667+danielh27@users.noreply.github.com>
2024-01-28 13:47:48 -0500

    Services list styles (#18)
    * feat: update list services view styles
    
    * feat: add link to service show to service cards
    
    * change card hover styles
    
    * feat: add service card component

--- app/components/user/avatar_component.rb
+++ app/components/user/avatar_component.rb
@@ -3,9 +3,11 @@
 class User::AvatarComponent < ApplicationComponent
   attr_reader :classes, :data, :user
 
-  def initialize(user, classes: nil, data: {})
+  def initialize(user, classes: nil, width: 40, height: 40,  data: {})
     @user = user
     @classes = classes
     @data = data
+    @width = width
+    @height = height
   end
 end
